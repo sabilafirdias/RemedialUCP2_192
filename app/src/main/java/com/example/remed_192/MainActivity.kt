bba0d1db1d788bc3ce24b740e04576dbfeb0559f
sabilafirdias <sabilafirdias@gmail.com>
2026-01-31 10:35:27 +0700

    selesai

--- app/src/main/java/com/example/remed_192/MainActivity.kt
+++ app/src/main/java/com/example/remed_192/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.remed_192.ui.theme.Remed_192Theme
+import com.example.remed_192.view.uicontroller.PerpustakaanApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             Remed_192Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    PerpustakaanApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
